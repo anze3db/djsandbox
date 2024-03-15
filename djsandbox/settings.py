e51d50eeb1570a5c5173bf42092bc7f88da575b4
Anže Pečar <anze@pecar.me>
2024-03-15 04:42:21 +0000

    Fix hostname in settings.py

--- djsandbox/settings.py
+++ djsandbox/settings.py
@@ -31,7 +31,7 @@ SESSION_COOKIE_SECURE = True
 
 CSRF_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = ["localhost", "sandbox.pecar.me"]
+ALLOWED_HOSTS = ["localhost", "djsandbox.pecar.me"]
 
 
 # Application definition
